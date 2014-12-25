24251b44810067cd9e4eebb7326ade831f5b9d9b
Ben Bowden <benbowdene@gmail.com>
2014-12-25 16:57:58 -0500

    Added names

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
  
 
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page])
   end
 
   def show
